f1bd9c12ae8993ae40fd118807aed8e983995b51
vungho <hoquocvung@gmail.com>
2025-06-16 16:23:04 +0000

    feat: add keyboard handler

--- src/components/todo-item/todo-item.tsx
+++ src/components/todo-item/todo-item.tsx
@@ -12,15 +12,24 @@ export function TodoItem({ taskId, title, completed, onToggle }: Partial<Task>) 
   };
 
   return (
-    <div className='d-block'>
-      <div className='h-2 w-full flex hover:cursor-pointer'>
-        <div className='h-2 w-2 p-2' onClick={() => onToggle && onToggle({ taskId })}>
-          <input type='checkbox' checked={isCompleted} onChange={() => comletedInputChange()} />
-        </div>
-        <div className='flex-auto h-2 p-2 pl-4'>
-          <p>{title}</p>
-        </div>
+    <li className='flex items-center justify-between p-3 bg-white rounded-lg shadow'>
+      <div className='flex items-center space-x-2'>
+        <input
+          type='checkbox'
+          className='todo-checkbox h-5 w-5 text-blue-600'
+          checked={isCompleted}
+          onChange={() => comletedInputChange()}
+        />
+        <span className='todo-text'>{title}</span>
       </div>
-    </div>
+      <div className='flex space-x-2'>
+        <button className='edit-todo text-yellow-500 hover:text-yellow-600'>
+          <i className='fas fa-edit'></i>
+        </button>
+        <button className='delete-todo text-red-500 hover:text-red-600'>
+          <i className='fas fa-trash'></i>
+        </button>
+      </div>
+    </li>
   );
 }
